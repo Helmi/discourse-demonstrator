2320ba32aefaee9fe6f1d7a00ac8d9a2ab2ea986
Ahmed Gagan <ahmedgagan6@gmail.com>
2021-12-03 15:49:25 +0530

    version increment

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-demonstrator
 # about: Send invites to new demonstrators
-# version: 0.1.8
+# version: 0.1.9
 # authors: Pfaffman, Helmi
 # url: https://github.com/helmi/discourse-demonstrator
 
